be05d1cecffdb59feff8a9b5c377df162599eba3
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-08-21 16:11:04 -0500

    Updated App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import VideoPlayer from './components/VideoPlayer';
+import SearchBar from './components/SearchBar/SearchBar';
+import Loader from './components/Loader';
 
 function App() {
+  const [url, setURL] =  useState("https://www.youtube.com/watch?v=JC90NBGC_bA");
+  const [isLoading, setIsLoading] = useState(true); // Initialize to true if data fetching starts on mount
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    let timer: any;
+    if (isLoading) {
+      timer = setTimeout(() => {
+        setIsLoading(false);
+      }, 1000); // 1 second
+    }
+
+    return () => {
+      clearTimeout(timer); // Cleanup: clear timeout if component unmounts or isLoading changes
+    };
+  }, []);
+
+  const handleInputChange = (event: { target: { value: any; }; }) => {
+    const { value } = event.target;
+    setInputValue(value);
+  };
+
+  const handleSubmit = () => {
+    let counter;
+    setIsLoading(true);
+    
+      counter = setTimeout(() => {
+        setIsLoading(false);
+        setURL(inputValue);
+    }, 1000); // 1 second  
+
+    setURL(inputValue);
+    return () => clearInterval(counter);
+  }
+
   return (
-    <div className="App">
+    <div className="App">      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h3>React Video Player Demo</h3>
+        <p><i>Enter link below</i></p>
+        <SearchBar  
+          value={inputValue} onChange={handleInputChange} onClick={()=>{handleSubmit()}}
+        />
+        <div style={{marginBottom:5}}></div>       
+        <div style={{color:"black",borderWidth:10, padding: 2,width:"640px",
+                    border:20,borderStyle:"solid", height:"360px" }}>
+          {!isLoading? 
+            <VideoPlayer 
+              url={url}
+            />
+          :
+            <div style={{paddingTop:50, paddingBottom:50}}>
+              <Loader/>
+            </div>
+          }
+        </div>  
+      </header>      
     </div>
   );
 }
---
+++ app/src/components/VideoPlayer.tsx
@@ -0,0 +1,18 @@
+import ReactPlayer from 'react-player';
+
+type VideoPlayerProps ={
+    url: any
+}
+const VideoPlayer : React.FC<VideoPlayerProps> = ({url})=> {
+  return (
+    <ReactPlayer
+    //   url={url}
+      controls={true}
+      width="640px"
+      height="360px"
+
+    />
+  );
+}
+
+export default VideoPlayer;
